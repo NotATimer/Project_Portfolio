66d109840b85d6f125c81ec6778553f03da1876e
NotATimer <alamreynatheer@gmail.com>
2026-04-24 15:16:02 +0800

    Refactor statistical calculator to use struct for data

--- C/Statistic Calculator.c
+++ C/Statistic Calculator.c
@@ -5,11 +5,17 @@
 #include <string.h>
 #include <ctype.h>
 
-void initialize /*Function for initializing values*/ (double **set, int *index) {
+struct data {
+    int index, num_modes;
+    double *set, *modes, *population_zscore, *sample_zscore;
+    double mean, median, population_variance, sample_variance;
+};
+
+void initialize /*Function for initializing values*/ (struct data *sheet) {
     char buffer[101]; //input buffer
     int multiple = 10;
     double* temp;
-    *set = (double *)malloc(sizeof(double) * multiple); //initial memory allocation
+    sheet->set = (double *)malloc(sizeof(double) * multiple); //initial memory allocation
     while(true) {
         fgets(buffer, 100, stdin);
         buffer[strcspn(buffer, "\n")] = '\0';
@@ -22,37 +28,44 @@ void initialize /*Function for initializing values*/ (double **set, int *index) 
         }
         else if(strcmp(buffer, "s") == 0 || strcmp(buffer, "S") == 0) { //for cancelling input
             printf("\nStopping\n");
-            if(*index == 0) {
-                *set = NULL;
+            if(sheet->index == 0) {
+                sheet->set = NULL;
             }
-            if(*index < multiple) {
-                temp = (double *)realloc(*set, sizeof(double) * *index);
+            else if(sheet->index < multiple) {
+                temp = (double *)realloc(sheet->set, sizeof(double) * sheet->index);
                 if(temp == NULL) {
                     printf("MEMORY ALLOCATION FAIL!");
                     exit(1);
                 }
                 else {
-                    *set = temp;
+                    sheet->set = temp;
                 }
             }
             break;
         }
         else if(strcmp(buffer, "d") == 0 || strcmp(buffer, "D") == 0) { //for removing previous instance
-            if(*index == 0) {
+            if(sheet->index == 0) {
                 printf("There is nothing to delete\n");
                 continue;
             }
             else {
                 printf("Deleted previous instance\n");
-                (*index)--;
+                (sheet->index)--;
                 continue;
             }
         }
         else if(strcmp(buffer, "r") == 0 || strcmp(buffer, "R") == 0) { //for resetting the entire stream
             printf("Stream Reset\n");
-            (*index) = 0;
+            (sheet->index) = 0;
             multiple = 10;
-            *set = (double *)realloc(*set, sizeof(double) * multiple);
+            temp = (double *)realloc(sheet->set, sizeof(double) * multiple);
+            if(temp == NULL) {
+                printf("MEMORY ALLOCATION FAILED!\n");
+                exit(1);
+            }
+            else {
+                sheet->set = temp;
+            }
             continue;
         }
         //checks if valid
@@ -75,17 +88,17 @@ void initialize /*Function for initializing values*/ (double **set, int *index) 
             while(isspace(*endptr)) { //checks for spaces
                 endptr++; //moves to the next column
             }
-            (*set)[*index] = value;
-            (*index)++;
-            if(*index == multiple) { //changes size of array if you went beyond
+            (sheet->set)[sheet->index] = value;
+            (sheet->index)++;
+            if(sheet->index == multiple) { //changes size of array if you went beyond
                 multiple *= 2;
-                temp = (double *)realloc(*set, sizeof(double) * multiple);
+                temp = (double *)realloc(sheet->set, sizeof(double) * multiple);
                 if(temp == NULL) {
                     printf("MEMORY ALLOCATION FAILED!");
                     exit(1);
                 }
                 else {
-                    *set = temp;
+                    sheet->set = temp;
                 }
             }
             if(*endptr == '\0') { //checks for the null terminator then ends loop
@@ -215,73 +228,73 @@ double* zscore(double set[], int index, double mean, double std) {
     return zscores;
 }
 
-void display(double set[], int index, double mean, double median, double modes[], int num_modes, double population_variance, double population_zscore[], double sample_variance, double sample_zscore[]) {
+void display(struct data sheet) {
     printf("\nResults\nSet (organized lowest to highest): ");
-    for(int h = 0;h<index;h++) { //loop that prints the arranged values of the array
-        printf("%.2lf ", set[h]);
+    for(int h = 0;h<sheet.index;h++) { //loop that prints the arranged values of the array
+        printf("%.2lf ", sheet.set[h]);
     }
-    if(index == 1) {
+    if(sheet.index == 1) {
         printf("\nPopulation Z-Scores: undefined");
     }
     else {
         printf("\nPopulation Z-Scores:");
-        for(int i = 0;i < index;i++) {
-            printf(" %.2lf", population_zscore[i]);
+        for(int i = 0;i < sheet.index;i++) {
+            printf(" %.2lf", sheet.population_zscore[i]);
         }
     }
-    if(index == 1) {
+    if(sheet.index == 1) {
         printf("\nSample Z-Scores: undefined");
     }
     else {
         printf("\nSample Z-Scores:");
-        for(int j = 0;j < index;j++) {
-            printf(" %.2lf", sample_zscore[j]);
+        for(int j = 0;j < sheet.index;j++) {
+            printf(" %.2lf", sheet.sample_zscore[j]);
         }
     }
-    printf("\nMean (μ): %.2lf\nMedian (x̃): %.2lf\nMode(s): ", mean, median);
-    if(num_modes == 0) {
+    printf("\nMean (μ): %.2lf\nMedian (x̃): %.2lf\nMode(s): ",sheet. mean,sheet. median);
+    if(sheet.num_modes == 0) {
         printf("No Mode");
     } else {
-        for(int k = 0;k < num_modes;k++) {
-            printf("%.2lf ", modes[k]);
+        for(int k = 0;k < sheet.num_modes;k++) {
+            printf("%.2lf ", sheet.modes[k]);
         }
     }
-    printf("\nMin: %.2lf\nMax: %.2lf\nRange: %.2lf", set[0], set[index - 1], (set[index - 1] - set[0]));
-    printf("\nPopulation Variance (σ²): %.2lf\nPopulation Standard Deviation (σ): %.2lf", population_variance, sqrt(population_variance));
-    if(index == 1) {
+    printf("\nMin: %.2lf\nMax: %.2lf\nRange: %.2lf", sheet.set[0], sheet.set[sheet.index - 1], (sheet.set[sheet.index - 1] - sheet.set[0]));
+    printf("\nPopulation Variance (σ²): %.2lf\nPopulation Standard Deviation (σ): %.2lf", sheet.population_variance, sqrt(sheet.population_variance));
+    if(sheet.index == 1) {
         printf("\nSample Variance (s²): undefined\nSample Standard Deviation: undefined");
     }
     else {
-        printf("\nSample Variance (s²): %.2lf\nSample Standard Deviation: %.2lf", sample_variance, sqrt(sample_variance));
+        printf("\nSample Variance (s²): %.2lf\nSample Standard Deviation: %.2lf", sheet.sample_variance, sqrt(sheet.sample_variance));
     }
 }
 
 int main() {
     char repeat;
-    double *set, *modes, *population_zscore, *sample_zscore; //the set and others
-    double mean, median, population_variance, sample_variance; //the outputs
+    struct data sheet;
     printf("Statistical calculator\n");
     do { //loops the program
-        int index = 0, num_modes = 0;
+        sheet.index = 0;
+        sheet.num_modes = 0;
         printf("\nInput the numbers(Enter 'h' for list of commands)\n");
-        initialize(&set, &index); //function for initializing
-        if(set == NULL) {
+        initialize(&sheet); //function for initializing
+        if(sheet.set == NULL) {
             printf("\nYou didn't input anything!");
         } else {
-            qsort(set, index, sizeof(double), sort); //sorts the array from lowest to highest
-            mean = MeanCalculate(set, index);
-            median = MedianCalculate(set, index);
-            modes = FindMode(set, index, &num_modes);
-            population_variance = VarianceCalculate(set, index, mean, false);
-            sample_variance = VarianceCalculate(set, index, mean, true);
-            population_zscore = zscore(set, index, mean, sqrt(population_variance));
-            sample_zscore = zscore(set, index, mean, sqrt(sample_variance));
-            display(set, index, mean, median, modes, num_modes, population_variance, population_zscore, sample_variance, sample_zscore);
-            free(modes);
-            free(population_zscore);
-            free(sample_zscore);
+            qsort(sheet.set, sheet.index, sizeof(double), sort); //sorts the array from lowest to highest
+            sheet.mean = MeanCalculate(sheet.set, sheet.index);
+            sheet.median = MedianCalculate(sheet.set, sheet.index);
+            sheet.modes = FindMode(sheet.set, sheet.index, &sheet.num_modes);
+            sheet.population_variance = VarianceCalculate(sheet.set, sheet.index, sheet.mean, false);
+            sheet.sample_variance = VarianceCalculate(sheet.set, sheet.index, sheet.mean, true);
+            sheet.population_zscore = zscore(sheet.set, sheet.index, sheet.mean, sqrt(sheet.population_variance));
+            sheet.sample_zscore = zscore(sheet.set, sheet.index, sheet.mean, sqrt(sheet.sample_variance));
+            display(sheet);
+            free(sheet.modes);
+            free(sheet.population_zscore);
+            free(sheet.sample_zscore);
         }
-        free(set);
+        free(sheet.set);
         printf("\n\nContinue using? (y to continue, anything else to stop): ");
         scanf(" %c", &repeat);
         while(getchar() != '\n');
