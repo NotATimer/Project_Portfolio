7f29bc546ed6a967a544b52d5a108552a9790e66
NotATimer <alamreynatheer@gmail.com>
2026-05-09 16:36:50 +0800

    First C++ Project
    Gonna use a Linked List instead, cuz I think using linked lists in C++ would be easier than in C

---
+++ C++/WeightedStatsCalc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+struct node {
+    node *prev, *next;
+    double value;
+    int weight;
+};
+
+int main() {
+    std::cout << "Weighted Stats calc\n\n";
+}
